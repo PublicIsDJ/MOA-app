219f10032d42cef9905a732305068cca4c9cc5a9
Minter <adf5858@naver.com>
2025-10-09 17:23:37 +0900

    feat: 테스트용 라우터 설정

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,13 +1,1 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { MissionPage } from '../../pages/cards/MissionPage'
-export function AppRouter() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/cards/CD-001" replace />} />
-        <Route path="/cards/:slug" element={<MissionPage />} />
-        <Route path="*" element={<Navigate to="/cards/CD-001" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+export { AppRouter } from '../../router'
