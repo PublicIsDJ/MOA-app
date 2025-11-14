3df6f095a0a7ee81f9852f41264fbb58b1176696
swtae0214 <20211937@edu.hanbat.ac.kr>
2025-11-15 03:50:50 +0900

    feat: time-009 페이지 작성

--- app/(cardThema)/time/(cards)/cd-009/page.tsx
+++ app/(cardThema)/time/(cards)/cd-009/page.tsx
@@ -7,11 +7,11 @@ import { Button } from '@/shared/ui/button';
 import { TIME_SEASON } from '@/features/cardThema/time/constant/season';
 import { TasteForm } from '@/features/cardThema/time/ui/season-form';
 import { FriendForm } from '@/features/cardThema/time/ui/friend-form';
+import { TIME_FRIEND, TIME_FRIEND_WITH } from '@/features/cardThema/time/constant/friend';
 
 export default function CardOfFriendPage() {
     const router = useRouter();
-    const [selectedSeason, setSelectedSeason] = useState<number | null>(null);
-    const [selectedTaste, setSelectedTaste] = useState<number | null>(null);
+    const [selectedFriend, setSelectedFriend] = useState<number | null>(null);
 
     return (
         <>
@@ -26,38 +26,19 @@ export default function CardOfFriendPage() {
             <div className='flex-1 flex flex-col gap-8 bg-[#F6F6F6] rounded-[20px] px-6 py-10'>
                 {/* MARK: 자연 */}
                 <FriendForm></FriendForm>
-                <section>
-                    <h1 className='text-lg font-semibold mb-4'>내가 좋아하는 계절은?</h1>
-                    <div className='grid grid-cols-2 gap-3'>
-                        {TIME_SEASON.season.map((item, index) => (
-                            <button
-                                key={index}
-                                type='button'
-                                onClick={() => setSelectedSeason(index)}
-                                className={`w-full aspect-[149/80] rounded-[12px] flex flex-col items-center justify-center gap-1 transition-all ${
-                                    selectedSeason === index
-                                        ? 'bg-[#4466D1] text-white'
-                                        : 'bg-white text-gray-900 hover:bg-gray-50'
-                                }`}
-                            >
-                                <p className='text-2xl'>{item.icon}</p>
-                                <p className='font-medium'>{item.title}</p>
-                            </button>
-                        ))}
-                    </div>
-                </section>
+                
 
                 {/* MARK: 동물 */}
                 <section>
                     <h1 className='text-lg font-semibold mb-4'>지인과 하고싶은것은?</h1>
                     <div className='grid grid-cols-3 gap-3'>
-                        {TIME_SEASON.taste.map((item, index) => (
+                        {TIME_FRIEND_WITH.map((item, index) => (
                             <button
                                 key={index}
                                 type='button'
-                                onClick={() => setSelectedTaste(index)}
+                                onClick={() => setSelectedFriend(index)}
                                 className={`w-full aspect-[149/80] rounded-[12px] flex flex-col items-center justify-center gap-1 transition-all ${
-                                    selectedTaste === index
+                                    selectedFriend === index
                                         ? 'bg-[#4466D1] text-white'
                                         : 'bg-white text-gray-900 hover:bg-gray-50'
                                 }`}
@@ -70,7 +51,6 @@ export default function CardOfFriendPage() {
                 </section>
             </div>
             
-            <TasteForm></TasteForm> {/* TODO: 글 내려쓰기 구현-> FormList 변경 해야 할 것 같아서 보류*/}
             <footer className='w-full flex gap-4 mt-5'>
                 <Button className='flex-1 py-[19px] rounded-[12px]' onClick={()=>router.push('/time/cd-010')}>다음으로</Button>
             </footer>
