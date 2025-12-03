1b6c85039a8790777b7ea6845ba957971a5b7fe6
Minter <adf5858@naver.com>
2025-12-03 23:40:28 +0900

    feat: 로그인폼 이벤트 핸들러 추가

--- src/features/auth/ui/login-form.tsx
+++ src/features/auth/ui/login-form.tsx
@@ -3,22 +3,24 @@ import { LoginFormState } from '../login/types';
 
 interface Props {
     formData: LoginFormState;
-    onUpdatedField: (filed: keyof LoginFormState, value: any) => void;
-
+    onUpdatedField: (field: keyof LoginFormState, value: string) => void;
 }
 
 export function LoginForm({ formData, onUpdatedField }: Props) {
     return (
-        <>
-            <div className='flex flex-col gap-[30px] w-full'>
-                <InputField
-                    placeholder='아이디 입력'
-                />
+        <div className='flex flex-col gap-[30px] w-full'>
+            <InputField
+                placeholder='아이디 입력'
+                value={formData.userId}
+                onChange={(e) => onUpdatedField('userId', e.target.value)}
+            />
 
-                <InputField
-                    placeholder='비밀번호 입력'
-                />
-            </div>
-        </>
+            <InputField
+                type='password'
+                placeholder='비밀번호 입력'
+                value={formData.password}
+                onChange={(e) => onUpdatedField('password', e.target.value)}
+            />
+        </div>
     );
 }
