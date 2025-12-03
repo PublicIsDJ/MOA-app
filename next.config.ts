10ee9bb4cc6bc81dd7c80c6b0315c29f265811a9
Minter <adf5858@naver.com>
2025-12-03 23:33:55 +0900

    fix: 임시 cors 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    'http://localhost:3000',
+  ],
+
+  // API 프록시 설정 (CORS 우회)
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://49.50.131.100:8000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
